f096867c98ac0919d7cb83a5726721ad936f8ed7
Hirok green <biswashirok@gmail.com>
2020-05-21 22:51:09 +0600

    added pgsql setting config (#3)
    Co-authored-by: hirok.biswas <hirok.biswas@evaly.com.bd>

--- src/parasite/parasite/settings.py
+++ src/parasite/parasite/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'parasite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': int(os.environ.get('DB_PORT', 5432)),
     }
 }
 
